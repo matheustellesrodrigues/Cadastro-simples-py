8030f3e551a8fb6d67ae422caaf7ac3183cc6de1
matheus <matheustelles@gmail.com>
2023-09-04 02:09:34 -0300

    Adicionado arquivo projeto.py

---
+++ projeto.py
@@ -0,0 +1,19 @@
+Cadastro = []
+while True: 
+    print("1 - Realizar Cadastro: ")
+    print("2 - Listar Cadastro: ")
+    print("3 - Sair: ")
+
+    opcao = int(input("Escolha uma opção: "))
+
+    if opcao == 1:
+        nome = input("Digite seu nome: ")
+        idade = int(input("Digite sua idade: "))
+        cpf = int(input("Digite seu cpf: "))
+
+        cadastro = {"nome": nome, "idade": idade, "cpf": cpf}
+        Cadastro.append(cadastro)
+    
+    if opcao == 2: 
+        for cadastro in Cadastro:
+            print(cadastro)
